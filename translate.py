d070c87a7ae36aee911676bdf557847bf36f3faf
rby011 <chsun.song@gmail.com>
2023-12-10 21:31:18 +0900

    Temp Saving

--- translate.py
+++ translate.py
@@ -1,17 +1,17 @@
 
 from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from dotenv import load_dotenv
+from typing import List
+from pprint import pprint
+import json
 
 def read_prompt_template(file_path: str) -> str:
     with open(file_path, "r") as f:
         prompt_template = f.read()
     return prompt_template
-
-input = '''Feeling hungry, we grabbed a pizza from the fridge and microwaved it.
-The video chronologically shows the son's finger stuck in a hole of the dog house, the rescue process by paramedics, and the conclusion of the entire incident.
-'''
 
 if __name__ == "__main__":
 
@@ -25,15 +25,19 @@ if __name__ == "__main__":
     # prompt_template = ChatPromptTemplate.from_template(
     #     template=read_prompt_template('prompts/1. translate.txt')
     # )
-    
 
+    src = {}
+    src["src"] = ["Feeling hungry, we grabbed a pizza from the fridge and microwaved it.", 
+                  "The video chronologically shows the son's finger stuck in a hole of the dog house, the rescue process by paramedics, and the conclusion of the entire incident."]
+    
     prompt_template = ChatPromptTemplate.from_messages( [
         ("system", read_prompt_template('prompts/1. translate.txt')),
-        ("human", input),
+        ("human", json.dumps(src).replace('{','{{').replace('}','}}')),
         ] )
 
     print(prompt_template)
 
+    # CHAINING
     chain = LLMChain(
         llm = llm, 
         prompt = prompt_template, 
@@ -41,9 +45,19 @@ if __name__ == "__main__":
         verbose = True
     )
     
+    # make prompt with template
     req = {}
     req['src_lang'] = 'english'
     req['dst_lang'] = 'korean'
+    
+    
+    # request 
     result = chain(req)
 
-    print(result['output'])
+    # check response
+    print("# Raw : ")
+    pprint(result)
+    
+    for it in result['output']['dst']:
+        print(it)
+    
