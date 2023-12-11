29fbb1078359ea68baa6d39dc633d3da224e42d1
changsun song <chsun.song@gmail.com>
2023-12-11 03:01:29 +0000

    skel_flow_finish

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ conda create -n python=3.10
 conda install openai -c conda-forge
 conda install langchain -c conda-forge
 conda install python-dotenv -c conda-forge
+pip install openpyxl
 ```
--- translate.py
+++ translate.py
@@ -1,63 +1,82 @@
 
 from langchain.chains import LLMChain
-from langchain.schema import BaseOutputParser
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from dotenv import load_dotenv
 from typing import List
 from pprint import pprint
 import json
+import pandas as pd
 
-def read_prompt_template(file_path: str) -> str:
-    with open(file_path, "r") as f:
-        prompt_template = f.read()
-    return prompt_template
+
 
 if __name__ == "__main__":
 
+    # 환경변수 로드 , esp. OPENAI_API_KEY
     load_dotenv()
-        
+    
+    # GPT-4 설정
     llm = ChatOpenAI(temperature=0.1, 
                      max_tokens=500, 
                      model="gpt-4",
                      verbose = True)
-        
-    # prompt_template = ChatPromptTemplate.from_template(
-    #     template=read_prompt_template('prompts/1. translate.txt')
-    # )
-
-    src = {}
-    src["src"] = ["Feeling hungry, we grabbed a pizza from the fridge and microwaved it.", 
-                  "The video chronologically shows the son's finger stuck in a hole of the dog house, the rescue process by paramedics, and the conclusion of the entire incident."]
     
+    # User prompt : 번역 대상 문장 목록
+    user_prompt = {}
+    user_prompt["src"] = ["Feeling hungry, we grabbed a pizza from the fridge and microwaved it.", 
+                  "The video chronologically shows the son's finger stuck in a hole of the dog house, the rescue process by paramedics, and the conclusion of the entire incident."]
+    user_prompt_str = json.dumps(user_prompt).replace('{','{{').replace('}','}}')
+
+    # System prompt : 번역 작업 지시
+    with open('prompts/1. translate.txt', 'r') as f:
+        sys_prompt_str = f.read()
+
+    # Prompt template : 번역 작업 지시 + 번역 대상 문장 목록
     prompt_template = ChatPromptTemplate.from_messages( [
-        ("system", read_prompt_template('prompts/1. translate.txt')),
-        ("human", json.dumps(src).replace('{','{{').replace('}','}}')),
+        ("system", sys_prompt_str),
+        ("human", user_prompt_str),
         ] )
 
     print(prompt_template)
 
-    # CHAINING
+    # Chaining with the above prompts
     chain = LLMChain(
         llm = llm, 
         prompt = prompt_template, 
         output_key = "output",
         verbose = True
     )
-    
-    # make prompt with template
+
+    # Make prompt with prompt template before making a request
     req = {}
     req['src_lang'] = 'english'
     req['dst_lang'] = 'korean'
     
-    
-    # request 
-    result = chain(req)
+    # Make a request to GPT
+    result_str = chain(req)
 
-    # check response
-    print("# Raw : ")
+    # Result Check
+    result = json.loads(result_str['output'])
     pprint(result)
+
+    # Compose result into dataframe with other info
+    src_lang = req['src_lang']
+    dst_lang = req['dst_lang']
+    src_sentences = user_prompt['src']
+    dst_sentences = result['dst']
+
+    columns = ['src_lang', 'dst_lang', 'sentence', 'translated']
+    df = pd.DataFrame(columns=columns)
+
+    for i in range(len(src_sentences)):
+        for j in range(3):
+            new_row = pd.DataFrame({'src_lang': [src_lang],
+                                    'dst_lang': [dst_lang],
+                                    'sentence': [src_sentences[i]],
+                                    'translated': [dst_sentences[i][j]]
+                                    })
+            df = pd.concat([df, new_row], ignore_index=True)
     
-    for it in result['output']['dst']:
-        print(it)
-    
+    print(df)
+
+    df.to_excel('result.xlsx', index=False)
